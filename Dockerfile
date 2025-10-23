dcd44ed98ba0d474aa05ed5c95572c0cbc9d83e1
Kirill <ivanovkirill12@gmail.com>
2025-10-24 01:09:16 +0300

    В докер файле отключен кеш модели, сборка на локальном компе проходит успешно, зависимости встали ровно. Запуск на локальном не проводился.

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,37 @@
-# Используем CUDA-окружение с поддержкой GPU
+
 FROM nvidia/cuda:12.1.1-runtime-ubuntu22.04
 
-# Устанавливаем Python, FFmpeg (для видео) и системные зависимости
+
 RUN apt update && apt install -y \
-    python3 python3-pip ffmpeg git wget \
+    python3 python3-pip ffmpeg git wget libcudnn8=8.9.2.26-1+cuda12.1 \
     && rm -rf /var/lib/apt/lists/*
 RUN apt-get update && apt-get install -y ffmpeg
-
+RUN pip install --no-cache-dir torch==2.2.1+cu121 torchvision==0.17.1+cu121 -f https://download.pytorch.org/whl/torch_stable.html
+RUN pip install --no-cache-dir xformers==0.0.23.post1 -f https://download.pytorch.org/whl/torch_stable.html
 WORKDIR /app
 
-# Копируем зависимости
+
+
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Загружаем модель Zeroscope v2 XL при сборке
-RUN python3 - <<'EOF'
-from diffusers import TextToVideoSDPipeline
-import torch
 
-print("⬇️ Загружаем модель Zeroscope v2 XL (Text-to-Video)...")
+#RUN python3 - <<'EOF'
+#from diffusers import TextToVideoSDPipeline
+#import torch
 
-pipe = TextToVideoSDPipeline.from_pretrained(
-    "cerspense/zeroscope_v2_XL",
-    torch_dtype=torch.float16
-)
+#print("⬇️ Загружаем модель Zeroscope v2 XL (Text-to-Video)...")
 
-print("✅ Модель закэширована в Hugging Face.")
-EOF
+#pipe = TextToVideoSDPipeline.from_pretrained(
+#    "cerspense/zeroscope_v2_XL",
+#    torch_dtype=torch.float16
+#)
 
-# Копируем серверный код
+#print("✅ Модель закэширована в Hugging Face.")
+#EOF
+
 COPY app.py .
 
-# Открываем порт для FastAPI
-EXPOSE 8000
+EXPOSE 8080
 
-# Запускаем FastAPI сервер
-CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8080"]
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from math import e
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from diffusers.pipelines.pipeline_utils import DiffusionPipeline
-import torch, uuid, imageio
+import torch, uuid
 from diffusers.pipelines.text_to_video_synthesis.pipeline_text_to_video_synth import TextToVideoSDPipeline
 from torchvision.io import write_video
 from pathlib import Path
 
+if torch.cuda.is_available():
+    device_name = torch.cuda.get_device_name(0)
+    vram_gb = torch.cuda.get_device_properties(0).total_memory / 1e9
+    print(f"GPU: {device_name}, VRAM: {vram_gb:.2f} GB")
+else:
+    print("⚠️ GPU не обнаружен — будет использован CPU")
 
 app = FastAPI()
 
@@ -15,53 +21,54 @@ output_dir.mkdir(exist_ok=True)
 
 
 print("🔄 Загружается модель...")
+
 try:
     pipe = TextToVideoSDPipeline.from_pretrained(
     "cerspense/zeroscope_v2_XL",
         torch_dtype=torch.float16
-    
     ).to("cuda")
+
     print("✅ Модель готова к работе!")
 
     # Оптимизация памяти и скорости
     pipe.enable_model_cpu_offload()
     pipe.enable_xformers_memory_efficient_attention()
-    pipe.to("cuda")
+    
 except Exception as exc:
     raise RuntimeError(f"Ошибка загрузки модели: {exc}")
+
+
+
 class VideoRequest(BaseModel):
     prompt: str
+
+
+
 
 @app.post("/generate")
 async def generate(req: VideoRequest):
     try:
-        print(f"GPU доступен: {torch.cuda.is_available()}")
-        print(f"Текущее устройство: {next(pipe.parameters()).device}")
+        
         print(f"🎬 Генерация: {req.prompt}")
-
+            
         # Генерация видео (T, H, W, C)
-        video_tensor = pipe(req.prompt, num_inference_steps=50).videos[0]
+        video_tensor = pipe(req.prompt, num_inference_steps=30).videos[0]
 
         # Перестановка осей (T, C, H, W) для torchvision
         video_tensor = video_tensor.permute(0, 3, 1, 2)
 
-
-        #video_tensor = pipe(req.prompt, num_frames=60).videos[0]  # Получаем тензор (T, H, W, C)
         filename = f"{uuid.uuid4().hex}.mp4"
         output_path = output_dir / filename
 
         # Сохранение видео с указанием FPS
         write_video(str(output_path), video_tensor, fps=8)
-
-
-        #imageio.mimsave(output_path, video_tensor, fps=8)
         print(f"✅ Видео сохранено: {output_path}")
-        #os.makedirs("output", exist_ok=True)
-        #path = f"output/{filename}"
-        #write_video(path, video_tensor.permute(0, 3, 1, 2), fps=8)  # Перемещаем каналы и задаем FPS
+        
         return {"video_path": str(output_path)}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Ошибка генерации видео: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Запрос поступил, но генерация не началась: {str(e)}")
+    
+    
 @app.get("/")
 async def read_root():
     return {"message": "Сервис генерации видео запущен! Используйте эндпоинт /generate."}
